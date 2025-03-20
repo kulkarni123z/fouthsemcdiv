ab0ab351c600d7577df1ca4355c1d60a7420aea5
kulkarni123z <kulkarnijanhavi00@gmail.com>
2025-03-20 11:36:05 +0530

    Add files via upload
    xyzz

---
+++ sud.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number: "))
+c=0
+a=0
+b=1
+if n==0 or n==1:
+    print("Yes it belongs to Fibonacci Sequence")
+else:
+  while c<n:
+        c=a+b
+        b=a
+        a=c
+if c==n:
+    print(n, " belongs to Fibonacci Sequence")
+else:
+    print(n, " does not belong to Fibonacci Sequence")
