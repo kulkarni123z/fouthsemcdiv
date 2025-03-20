8e91881044d008ff2924beab4186081979272896
kulkarni123z <kulkarnijanhavi00@gmail.com>
2025-03-20 11:30:48 +0530

    python project
    python files uploaded

---
+++ gautami.py
@@ -0,0 +1,9 @@
+"""
+import random
+random.seed(12)
+a = random.randint(10, 20)
+"""
+if 10>20:
+    print("chandu")
+else:
+    print("vardhaman")
---
+++ gul.py
@@ -0,0 +1,4 @@
+print("Enter a number")
+a = input()
+b = input("Enter another number")
+print("Sum is " , (a+b))
